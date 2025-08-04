958d41d06bfc4f588d477267bdd70060f9d737ab
UdonCrab <hz322@uw.edu>
2025-08-04 13:26:37 -0700

    update fielddipolemultiply

--- Dielectric/PotentialWrapper.cuh
+++ Dielectric/PotentialWrapper.cuh
@@ -43,6 +43,7 @@ public:
     Scalar *d_conductivity; // particle conductivity
     int *d_group_membership_tag; // particle membership and index in group for which the force calculation is being performed
     unsigned int *d_group_members;  // index into particle tag
+    unsigned int *d_rtag;
     const unsigned int *d_n_neigh;  // number of neighbors of each particle
     const unsigned int *d_nlist;    // neighbor list
     const unsigned int *d_head_list;  // used to access entries in the neighbor list
