0c3feea1ba7ab766efc679a4c59b315d76b2c2e3
UdonCrab <hz322@uw.edu>
2025-08-05 15:05:20 -0700

    Hmm I think i is idx, idx is tag here.

--- Dielectric/Dielectric.cc
+++ Dielectric/Dielectric.cc
@@ -732,7 +732,7 @@ void Dielectric::OutputData(unsigned int timestep) {
 		if (idx >= m_Ntotal) continue;
 
 		// Get the particle's position
-		Scalar4 postype = h_pos.data[idx];
+		Scalar4 postype = h_pos.data[i];
 
 		// Write the position to file
 		file << std::setprecision(10) << postype.x << "  " << postype.y << "  " << postype.z << "  " << i << "  " << idx << "  " << std::endl;
@@ -774,7 +774,7 @@ void Dielectric::OutputData(unsigned int timestep) {
 		if (idx >= m_Ntotal) continue;
 
 		// Get the particle's electric/magnetic force
-		Scalar4 force = h_force.data[idx];
+		Scalar4 force = h_force.data[i];
 
 		// Write the dipole to file
 		file << std::setprecision(10) << force.x << "  " << force.y << "  " << force.z << "  " << i << "  " << idx << "  " << std::endl;
